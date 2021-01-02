c9f087956f5a1039020084aa942289e4beed9809
salter19 <salonenterhi19@gmail.com>
2021-01-02 16:20:17 +0200

    Set eslint & prettier for project.

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+/* eslint-disable comma-dangle */
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'prettier', 'prettier/standard'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: ['prettier'],
+  rules: {
+    
+  }
+}
