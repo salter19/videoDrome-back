b0dd47ac7e5e5b82715bb61a47f15fbedabf75bf
salter19 <salonenterhi19@gmail.com>
2021-01-02 17:14:02 +0200

    Add to index: server and connection try-catch.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+const path = require('path');
+const DB =  require(path.join(__dirname, './routes/cruderepository.js'));
 const EXPRESS = require('express');
 const APP = EXPRESS();
 const CORS = require('cors');
 
 APP.use(CORS());
 
+const port = 8080;
+
+const server = APP.listen(port, async () => {
+  try {
+    await DB.connect();
+    console.log(`Listening on port ${server.address().port}`);
+  } catch (err) {
+    console.log(err);
+    server.close();
+  }
+});
