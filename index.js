1ff6347cd5fceddc28954b8dbd339808324b6d75
salter19 <salonenterhi19@gmail.com>
2021-01-02 17:35:27 +0200

    Test connection in index.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const DB =  require(path.join(__dirname, './routes/cruderepository.js'));
+const DB =  require(path.join(__dirname, './routes/crudrepository.js'));
 const EXPRESS = require('express');
 const APP = EXPRESS();
 const CORS = require('cors');
@@ -8,12 +8,14 @@ APP.use(CORS());
 
 const port = 8080;
 
+
 const server = APP.listen(port, async () => {
   try {
     await DB.connect();
     console.log(`Listening on port ${server.address().port}`);
   } catch (err) {
-    console.log(err);
+    console.log(err + ' catch here');
+    await DB.close();
     server.close();
   }
 });
