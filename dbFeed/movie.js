77aca1823c6518bcc76fe3756918bf662be6cc30
salter19 <salonenterhi19@gmail.com>
2021-01-05 15:22:47 +0200

    Refactor movie: construct differently regarding where the call comes from.

--- dbFeed/movie.js
+++ dbFeed/movie.js
@@ -1,19 +1,51 @@
+const splitter = (data) => {
+  if (typeof data === 'string') {
+    const re = /\s*(?:,|$)\s*/;
+
+    return data.split(re);
+
+  }
+}
+
 class Movie {
-  constructor(data) {
+  constructor(data, isOMDB) {
+    console.log('coming from omdb? ' + isOMDB)
+    isOMDB ? this.setFromOMDB(data) : this.setFromApp(data);
+
+  }
+
+  setFromOMDB(data) {
     this.name = data.Title;
     this.category = data.Type;
-    this.genres = data.Genre.split(', ');
+    this.genres = splitter(data.Genre);
     this.year = data.Year;
-    this.director = data.Director.split(', ');
-    this.country = data.Country.split(', ');
-    this.sub = data.Subs.split(', ');
+    this.director = splitter(data.Director);
+    this.country = splitter(data.Country);
+    this.sub = splitter(data.Subs)
     this.format = '';
     this.format2 = '';
-    this.actors = data.Actors.split(', ');
+    this.actors = splitter(data.Actors);
+    this.imdbID = data.imdbID;
+  }
+
+  setFromApp(data) {
+
+    this.name = data.name;
+    this.category = data.category;
+    this.genres = splitter(data.genres);
+    this.year = data.year;
+    this.director = splitter(data.director);
+    this.country = splitter(data.country);
+    this.sub = splitter(data.sub);
+    this.format = data.format;
+    this.format2 = data.format2;
+    this.actors = splitter(data.actors);
     this.imdbID = data.imdbID;
   }
 
   getCard() {
+    console.log('heres return')
+    console.log(this)
     return this;
   }
 
