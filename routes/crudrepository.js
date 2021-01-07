8da841e5b1c6b3273cb4be3290d1ad5afd389760
salter19 <salonenterhi19@gmail.com>
2021-01-07 14:18:51 +0200

    Add to crudrepository: addNewItem() & genres, country & sub id getters.

--- routes/crudrepository.js
+++ routes/crudrepository.js
@@ -1,4 +1,5 @@
 const { error } = require('console');
+const { get } = require('http');
 const mysql = require('mysql');
 const path = require('path');
 const config = require(path.join(__dirname, './../dbml/config.js'));
@@ -41,16 +42,29 @@ const getIDs = async(table, col, items) => {
   }
 
   const arr =  await items.length > 1 ? await getMany() : await getOne();
+  
   return arr;
 };
 
 const mapArr = async (arr, table, col) => {
 
   const res = [];
+  let newID;
+  let id; 
 
-  for (const item of arr) {
-    const id = await connectionFunctions.getItemId(table, col, item);
-    res.push(id[0].id);
+  for (let item of arr) {
+    if (table === 'genres' || table === 'formats' || table === 'category') {
+      item = item.toLowerCase();
+    } 
+    const idIs = await connectionFunctions.getItemId(table, col, item).catch(error);
+   
+    if (idIs === undefined) {
+      newID = await connectionFunctions.saveNewItem(table, col, item).catch(error);
+      id = newID
+    } else {
+      id = idIs[0].id;
+    }
+    res.push(id);
   }
   return res;
 };
@@ -134,9 +148,11 @@ const connectionFunctions = {
         const title = 'title';
         const catID = await getID('categories', title, article.category).catch(error);
         const formatIDs = await getIDs('formats', title, article.format).catch(error);
-        console.log('at crud / saveToDatabase:')
-        console.log(formatIDs)
-        console.log(catID)
+        const genresIDs = await getIDs('genres', title, article.genres).catch(error);
+        const countryIDs = await getIDs('countries', title, article.country).catch(error);
+        const subIDs = await getIDs('subtitles', title, article.sub).catch(error);
+        
+        console.log(subIDs)
         resolve(article);
       }
 
@@ -155,13 +171,39 @@ const connectionFunctions = {
         : reject(status.notFound + ' - not found.');
       }
 
-      try {
-        connection.query(sql, item, (err, res) => {
-          err ? reject(err) : success(res);
-        })
-      } catch (error) {
-        reject(error);
+      const innerFunc = () => {
+        try {
+          connection.query(sql, item, (err, res) => {
+            err ? reject(err) : success(res);
+          })
+        } catch (error) {
+          reject(error);
+        }
       }
+
+      connection ? innerFunc() : reject(serverError);
+    }
+    return new Promise(func);
+  }, 
+
+  saveNewItem: (table, col, item) => {
+
+    const sql = `INSERT INTO ${table} (${col}) VALUES ( ? );`
+    const func = (resolve, reject) => {
+      const innerFunc = async () => {
+        const success = (res) => {
+          resolve(res.insertId);
+        }
+        try {
+          connection.query(sql, item, (err, res) => {
+            err ? reject(status.userErr + ' - Invalid input.') : success(res);
+          })
+          
+        } catch (error) {
+          reject(status.userErr + ' - something went wrong with item save. Invalid value.')
+        }
+      }
+      connection ? innerFunc() : reject(serverError);
     }
     return new Promise(func);
   }
